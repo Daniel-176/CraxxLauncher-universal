ffcb4534285053eb879d83876e9d69d4df4cdacf
Daniel-176 <danielftosta@hotmail.com>
2026-08-30 16:20:36 -0300

    basic logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
+int installer(craxxblox& config) {
 
+    return 1;
+}
 
-int main() {
+int launcher(craxxblox& config, int argc, char* argv[]) {
+
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    cout << "CRAXXBLOX Launcher!";
+    craxxblox config;
+
+    if(argc < 2) {
+        installer(config);
+    } else {
+        launcher(config, argc, argv);
+    }
+
     return 0;
 }
